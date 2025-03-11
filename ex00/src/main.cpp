499cc3632b1adfaab0e7c386bf829b651d711b2b
Samuel Hurtado <lnx.wifi@gmail.com>
2025-03-11 16:38:16 +0100

    compilable with shared library

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,11 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
+/*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 11:54:47 by shurtado          #+#    #+#             */
-/*   Updated: 2025/03/09 11:54:55 by shurtado         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:35:59 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat/Bureaucrat.hpp"
+
+int main()
+{
+	return 1;
+}
